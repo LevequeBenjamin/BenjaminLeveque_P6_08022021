261619ed62039107f54548982d8d5e44c1d391a8
bosso74 <bosso27@live.fr>
2021-02-09 17:25:21 +0100

    PB gitignore

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const path = require('path');
+
+const saucesRoutes = require('./routes/sauces');
+
+const userRoutes = require('./routes/user');
+
+//const cors = require('cors');
+
+require('dotenv').config();
+
+mongoose
+	.connect(process.env.DB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => console.log('Connexion à MongoDB réussie !'))
+	.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+//app.use(cors({ origin: 'http://localhost:4200' }));
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+	);
+	next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+	email: {
+		type: String,
+		required: [true, 'Veuillez entrer votre adresse email'],
+		match: [
+			/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+			'Veuillez entrer une adresse email correcte',
+		],
+	},
+	password: {
+		type: String,
+		required: [true, 'Veuillez choisir un mot de passe'],
+	},
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
